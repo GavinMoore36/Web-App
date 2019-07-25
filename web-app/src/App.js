08fbab3f3f4a8d067639c016dee9a9a0e94bd92e
gavinmoore36 <gavizoid16@gmail.com>
2019-07-24 22:07:30 -0700

    Added bootstrap and a Navbar

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavGuide from './components/navbar.js'
 
 function App() {
   return (
     <div className="App">
+    <NavGuide />
       <header className="App-header">
       <img className='Home-image' alt="Box-Home" src='https://i.etsystatic.com/17607550/r/il/0dbd11/1917628920/il_1588xN.1917628920_4a9m.jpg'/>
         <p>
---
+++ web-app/src/components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+const NavGuide = (props) => {
+
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="home">Lori's Containment</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="home">Home</Nav.Link>
+          <Nav.Link href="info">Info</Nav.Link>
+          <Nav.Link href="products">Products</Nav.Link>
+          <Nav.Link href="contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar>
+    )
+  }
+
+export default NavGuide;
