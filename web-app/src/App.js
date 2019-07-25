21e2ecda331ebaa6906d49b4d2a3146b0d077269
gavinmoore36 <gavizoid16@gmail.com>
2019-07-24 21:43:13 -0700

    Added image and changed the css

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img className='Home-image' alt="Box-Home" src='https://i.etsystatic.com/17607550/r/il/0dbd11/1917628920/il_1588xN.1917628920_4a9m.jpg'/>
         <p>
           Welcome to Lori's Containment
         </p>
